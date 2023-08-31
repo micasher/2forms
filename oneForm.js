ea005697d2e58fa3d3a2a64dcfdddd8793bfe241
micasher <121874049+micasher@users.noreply.github.com>
2023-08-31 13:04:41 +0300

    first commit

---
+++ oneForm.js
@@ -0,0 +1,21 @@
+// const settingsButton = document.getElementById("settingsButton");
+// const settingsPanel = document.getElementById("settingsPanel");
+// const mainHeader = document.getElementById("mainHeader");
+// const colorOption = document.getElementById("colorOption");
+
+// settingsButton.addEventListener("click", () => {
+//   settingsPanel.classList.toggle("hidden");
+// });
+
+// colorOption.addEventListener("change", () => {
+//   const selectedColor = colorOption.value;
+//   mainHeader.style.color = selectedColor; // Update header's color style
+//   const colorSetting = `color: ${selectedColor};`; // Create the CSS setting
+//   updateSettingDisplay(colorSetting); // Update displayed settings
+// });
+
+// // Function to update displayed settings
+// function updateSettingDisplay(setting) {
+//   const settingsDisplay = document.getElementById("settingsDisplay");
+//   settingsDisplay.textContent = setting;
+// }
