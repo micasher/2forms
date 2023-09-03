30392b726ac525d8207233277e51056f64fcbd74
micasher <121874049+micasher@users.noreply.github.com>
2023-09-03 15:49:48 +0300

    03.09

--- oneForm.js
+++ oneForm.js
@@ -1,21 +1,59 @@
-// const settingsButton = document.getElementById("settingsButton");
-// const settingsPanel = document.getElementById("settingsPanel");
-// const mainHeader = document.getElementById("mainHeader");
-// const colorOption = document.getElementById("colorOption");
+document.addEventListener("DOMContentLoaded", function () {
+  const settingsButton = document.getElementById("settingsButton");
+  const settingsMenu = document.getElementById("Settings");
 
-// settingsButton.addEventListener("click", () => {
-//   settingsPanel.classList.toggle("hidden");
-// });
+  settingsButton.addEventListener("click", function () {
+    if (
+      settingsMenu.style.display === "none" ||
+      settingsMenu.style.display === ""
+    ) {
+      settingsMenu.style.display = "block";
+    } else {
+      settingsMenu.style.display = "none";
+    }
+  });
 
-// colorOption.addEventListener("change", () => {
-//   const selectedColor = colorOption.value;
-//   mainHeader.style.color = selectedColor; // Update header's color style
-//   const colorSetting = `color: ${selectedColor};`; // Create the CSS setting
-//   updateSettingDisplay(colorSetting); // Update displayed settings
-// });
+  const CurrencyOptions = document.getElementById("CurrencyOptions");
+  const Language = document.getElementById("Language");
+  const VATType = document.getElementById("VATType");
+  const RoundingSum = document.getElementById("RoundingSum");
 
-// // Function to update displayed settings
-// function updateSettingDisplay(setting) {
-//   const settingsDisplay = document.getElementById("settingsDisplay");
-//   settingsDisplay.textContent = setting;
-// }
+  CurrencyOptions.addEventListener("change", function () {
+    const selectedCurrency = CurrencyOptions.value;
+    // Update the selected currency header
+    const selectedCurrencyHeader = document.getElementById(
+      "selectedCurrencyHeader"
+    );
+    selectedCurrencyHeader.textContent =
+      "Selected Currency: " + selectedCurrency;
+  });
+
+  Language.addEventListener("change", function () {
+    const selectedLanguage = Language.value;
+    // Update the selected language header
+    const selectedLanguageHeader = document.getElementById(
+      "selectedLanguageHeader"
+    );
+    selectedLanguageHeader.textContent =
+      "Selected Language: " + selectedLanguage;
+  });
+
+  VATType.addEventListener("change", function () {
+    const selectedVATType = VATType.value;
+    // Update the selected VAT type header
+    const selectedVATTypeHeader = document.getElementById(
+      "selectedVATTypeHeader"
+    );
+    selectedVATTypeHeader.textContent = "Selected VAT Type: " + selectedVATType;
+  });
+
+  RoundingSum.addEventListener("change", function () {
+    const selectedRoundingSum = RoundingSum.value;
+    // Update the selected rounding sum header
+    const selectedRoundingSumHeader = document.getElementById(
+      "selectedRoundingSumHeader"
+    );
+    selectedRoundingSumHeader.textContent =
+      "Selected Rounding Sum: " + selectedRoundingSum;
+  });
+});
